cf5a38e9ebc1f120880a67651c3ef94a1a7942a1
Andrew Hoang <andrewhoang03@gmail.com>
2017-10-16 13:00:43 -0700

    added in vocabulary creation

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,60 @@
 import torch
 import torch.autograd as autograd
 import torchvision.datasets as datasets
+import os.path
+
+def get_file_information():
+    image_dir = ""
+    annotation_dir = ""
+    while os.path.isdir(image_dir) == False:
+        print "image directory:",
+        image_dir = raw_input()
+    while os.path.isfile(annotation_dir) == False:
+        print "annotation directory:",
+        annotation_dir = raw_input()
+    build_vocab = os.path.isfile("vocab.txt") == False
+    vocab_input = ""
+    while build_vocab == False and vocab_input != "y" and vocab_input != "n":
+        print "rebuild vocabulary? (y/n)",
+        build_vocab = raw_input()
+    if vocab_input != "":
+        build_vocab = vocab_input == "y"
+    return image_dir, annotation_dir, build_vocab
+
+# input the vocab that contains the words, i.e index_to_word
+def write_vocab_to_file(vocab):
+    vocab_file = open('vocab.txt', 'w')
+    for word in vocab:
+        vocab_file.write(word + "\n")
+    vocab_file.close()
 
 def load_data(images, annotations, transform):
     return datasets.CocoCaptions(root = images, annFile = annotations, transform = transform)
 
+# Needs an array of images
 def image_to_variable(image):
     if torch.cuda.is_available():
         image = image.cuda()
     return autograd.Variable(image)
+
+# assumes that data is in form of a two tuple, with captions as the second
+def create_vocab(data, min_occurrence=1, unknown_val=0, end_of_seq_val=1, end_val=1):
+    word_to_index = {}
+    index_to_word = []
+    word_to_appearences = {}
+    word_to_index["UNK"] = unknown_val
+    index_to_word.append("UNK")
+    iter_number = 0
+    for _, captions in data:
+        if iter_number % 1000 == 0 and iter_number > 999:
+            print iter_number
+        for sentence in captions:
+            for word in sentence.split():
+                if word not in word_to_appearences:
+                    word_to_appearences[word] = 0
+                word_to_appearences[word] += 1
+                if word_to_appearences[word] == min_occurrence:
+                    word_to_index[word] = len(index_to_word)
+                    index_to_word.append(word)
+        iter_number += 1
+    return word_to_index, index_to_word
--- run.py
+++ run.py
@@ -3,11 +3,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import torchvision.models as models
+import sys
 import data_loader
 from lstm import LSTM
+
+def to_var(x):
+    return autograd.Variable(x.cuda())
 
 # defining image size
 transform = transforms.Compose([
@@ -19,18 +22,36 @@ transform = transforms.Compose([
         std=[0.229, 0.224, 0.225])
 ])
 
-training_set = data_loader.load_data(images='data/train2014', annotations='data/captions_train2014.json', transform=transform)
+image_dir, annotation_dir, build_vocab = data_loader.get_file_information()
+# training_set = data_loader.load_data(images='data/train2014', annotations='data/captions_train2014.json', transform=transform)
+training_set = data_loader.load_data(images=image_dir, annotations=annotation_dir, transform=transform)
 
-cnn_encoder = models.vgg16(pretrained=True)
-image, targe = training_set[3]
+cnn_encoder = models.vgg16(pretrained=True).cuda() if torch.cuda.is_available() else (models.vgg16(pretrained=True))
+image, target = training_set[3]
 images = []
 images.append(image)
 images = torch.stack(images, 0)
 print(images.size())
 # output is a feature vector of size 1000
 print(cnn_encoder(data_loader.image_to_variable(images)))
+img2, tar2 = training_set[100]
+print(img2.size())
+#print(cnn_encoder(to_var(images)))
+
+'''
+# creating the model
+batch_size, min_occurrences = 32, 10
+D_embed, H, D_out = 30, 100,30
+#model = LSTM(D_embed, H, len(word_to_index), batch_size).cuda() if torch.cuda.is_available() else LSTM(D_embed, H, len(word_to_index), batch_size)
+model = LSTM(D_embed, H, 1000, batch_size).cuda() if torch.cuda.is_available() else LSTM(D_embed, H, 1000, batch_size)
+loss_function = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+'''
 
 print('Number of samples: ', len(training_set))
 img, target = training_set[3]
 print("Image Size: ", img.size())
 print(target)
+
+word_to_index, index_to_word  = data_loader.create_vocab(training_set, min_occurrence=10)
+data_loader.write_vocab_to_file(index_to_word)
